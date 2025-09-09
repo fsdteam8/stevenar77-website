74f0be8aeb38fd80365347251a6a2571f3fb5e02
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-09-10 05:03:09 +0600

    terms hero

---
+++ app/(website)/privacy-pollicy/page.tsx
@@ -0,0 +1,14 @@
+import PrivacyHero from "@/components/website/privacypollicy/PrivacyHero";
+import PrivacyPollicy from "@/components/website/privacypollicy/PrivacyPollicy";
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <PrivacyHero />
+      <PrivacyPollicy />
+    </div>
+  );
+};
+
+export default page;
