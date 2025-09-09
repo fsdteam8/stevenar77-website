551b828dfe40c8b79d8af92c018774c7f8888d96
iftykhar <s.m.ifty49@gmail.com>
2025-09-09 07:24:37 +0600

    rescue divers

--- app/(website)/courses/page.tsx
+++ app/(website)/courses/page.tsx
@@ -1,9 +1,16 @@
+"use client"
 import StillHaveQuestion from '@/components/website/reusable/stillHaveQuestion'
+import Hero from '@/components/website/shared/Hero'
 import React from 'react'
 
 const page = () => {
   return (
     <div>
+      <Hero 
+      title="All Scuba Courses"
+      subtitle="Start your underwater adventure with our comprehensive beginner courses. From your first breath underwater to becoming a certified diver."
+      backgroundImage="/images/imagewater.jpg"
+      />
         <StillHaveQuestion />
     </div>
   )
