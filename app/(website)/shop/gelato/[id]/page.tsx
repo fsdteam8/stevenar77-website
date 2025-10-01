897fb5b805cc3927677ffd6d0c2152ed1b6fa34f
Md.Habibulla <mdhabibulla146425@gmail.com>
2025-10-02 04:52:08 +0600

    gelato product add

---
+++ app/(website)/shop/gelato/[id]/page.tsx
@@ -0,0 +1,33 @@
+// import Hero from '@/components/website/shared/Hero';
+import GelatoDetails from "@/components/website/shop/GelatoDetails";
+import ProductCarousel from "@/components/website/shop/ProductCarousel";
+import ProductDetails from "@/components/website/shop/ProductDetails";
+// import ProductReview from '@/components/website/shop/ProductReview';
+// import ProductReviewDes from '@/components/website/shop/ProductReviewDes';
+import React from "react";
+
+interface PageProps {
+  params: { id: string }; // dynamic segment e.g. /shop/product/[id]
+}
+
+export default function Page({ params }: PageProps) {
+  const productId = params.id;
+
+  // TODO: Replace with real userId from auth (e.g. NextAuth session)
+
+  return (
+    <div className="bg-[#FFFEFD]">
+      {/* <Hero
+        title="Gear Up for Your Next Dive"
+        subtitle="High-quality scuba equipment, apparel, and accessories — tested and trusted by divers worldwide. From masks to full dive computers, find everything you need for your underwater adventures."
+        backgroundImage="/images/imagewater.jpg"
+        size="small"
+      /> */}
+      <GelatoDetails />
+      {/* <ProductDetails /> */}
+      {/* <ProductReviewDes   /> */}
+      {/* <ProductReview userId={userId} productId={productId} /> */}
+      <ProductCarousel />
+    </div>
+  );
+}
---
+++ components/website/shared/GelatoShopProductCard.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { Button } from "@/components/ui/button";
+import { Star } from "lucide-react";
+import { GelatoShopProductCardType } from "@/services/hooks/product/useGelatoProducts";
+
+const GelatoShopProductCard: React.FC<GelatoShopProductCardType> = ({
+  image,
+  title,
+  description,
+  rating,
+  reviews,
+  price,
+  onSeeMore,
+  onBookNow,
+}) => {
+  return (
+    <div className="w-full h-full max-w-sm mx-auto rounded-2xl overflow-hidden shadow-md bg-white flex flex-col">
+      {/* Image */}
+      <div className="relative w-full h-56">
+        <Image src={image} alt={title} fill className="object-cover" priority />
+      </div>
+
+      <div className="bg-[#F8F9FA] py-2 flex-1 flex flex-col justify-between">
+        {/* Content */}
+        <div className="flex flex-col  flex-1 p-4">
+          <h3 className="text-lg font-semibold text-gray-800">{title}</h3>
+          <p className="text-sm text-gray-500 mt-1">{description}</p>
+
+          {/* Rating */}
+          <div className="flex items-center gap-1 mt-2 text-sm text-gray-600">
+            <Star className="w-4 h-4 text-yellow-500 fill-yellow-500" />
+            <span className="font-medium">{rating.toFixed(1)}</span>
+            <span className="text-gray-400">({reviews} reviews)</span>
+          </div>
+
+          {/* Price */}
+          {price == 0 ?  <div className="pt-2"> 
+               <Image src={`/images/gelato.svg`} alt="gelato" width={70} height={70} />
+             </div>  : (
+            <p className="mt-3 text-2xl font-semibold text-gray-800">
+              ${price}
+            </p>
+          )}
+        </div>
+
+        {/* Actions */}
+        <div className="flex gap-3 px-4">
+          <Button
+            variant="outline"
+            className="flex-1 text-base font-medium text-[#0694A2]"
+            onClick={onSeeMore}
+          >
+            See Details
+          </Button>
+          <Button
+            className="flex-1 bg-[#0694A2] text-base font-medium text-white hover:bg-cyan-700"
+            onClick={onSeeMore}
+          >
+            Draft Now
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GelatoShopProductCard;
