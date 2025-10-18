dd62f1d0471dc4f969963e277e4b3d258a85ee94
iftykhar <s.m.ifty49@gmail.com>
2025-10-18 06:48:47 +0600

    payment

--- app/(website)/shop/payment/cancel/page.tsx
+++
@@ -1,9 +0,0 @@
-// // app/payment/cancel/page.tsx  (Next.js App Router)
-// import ShopPaymentCancel from '@/components/website/reusable/ShopPaymentCancle';
-// import React from 'react';
-
-// const PaymentCancelPage = () => {
-//   return <ShopPaymentCancel />;
-// };
-
-// export default PaymentCancelPage;
