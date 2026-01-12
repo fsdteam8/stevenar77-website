3c287d577bb12a5a7a8fc8eed42ed38a843a0cdd
iftykhar <s.m.ifty49@gmail.com>
2026-01-13 00:33:30 +0600

    fix: wording fix

--- components/website/landing/WhyLearnUs.tsx
+++ components/website/landing/WhyLearnUs.tsx
@@ -7,7 +7,7 @@ const WhyLearnUs = () => {
     <div className="mt-[60px] lg:mt-[80px] mx-auto container">
       <div className="mb-[30px]  text-center">
         <h2 className="text-[#252F3F] text-[48px]  leading-[120%] pb-2">
-          How It Works
+          Why Learn with Us
         </h2>
         <p className="text-[16px] text-[#6B7280] text-center w-[90%] mx-auto leading-6 font-normal">
           Experience the difference that makes Scuba Life the premier choice for diving education
